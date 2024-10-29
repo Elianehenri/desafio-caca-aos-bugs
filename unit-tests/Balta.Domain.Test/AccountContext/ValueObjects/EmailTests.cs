4c6e9fe572c8a2e5da88628dca040e8f9e5a8e07
Eliane Henriqueta <elianehenriqueta@gmail.com>
2024-10-29 14:59:40 -0300

    Tests do email.

--- unit-tests/Balta.Domain.Test/AccountContext/ValueObjects/EmailTests.cs
+++ unit-tests/Balta.Domain.Test/AccountContext/ValueObjects/EmailTests.cs
@@ -1,34 +1,154 @@
-namespace Balta.Domain.Test.AccountContext.ValueObjects;
+using Balta.Domain.AccountContext.ValueObjects;
+using Balta.Domain.AccountContext.ValueObjects.Exceptions;
+using Balta.Domain.SharedContext.Abstractions;
+using Balta.Domain.SharedContext.Extensions;
+using Moq;
 
-public class EmailTests
+
+namespace Balta.Domain.Test.AccountContext.ValueObjects
 {
-    [Fact]
-    public void ShouldLowerCaseEmail() => Assert.Fail();
-    
-    [Fact]
-    public void ShouldTrimEmail() => Assert.Fail();
-    
-    [Fact]
-    public void ShouldFailIfEmailIsNull() => Assert.Fail();
-    
-    [Fact]
-    public void ShouldFailIfEmailIsEmpty() => Assert.Fail();
-    
-    [Fact]
-    public void ShouldFailIfEmailIsInvalid() => Assert.Fail();
-    
-    [Fact]
-    public void ShouldPassIfEmailIsValid() => Assert.Fail();
-    
-    [Fact]
-    public void ShouldHashEmailAddress() => Assert.Fail();
-    
-    [Fact]
-    public void ShouldExplicitConvertFromString() => Assert.Fail();
-    
-    [Fact]
-    public void ShouldExplicitConvertToString() => Assert.Fail();
-    
-    [Fact]
-    public void ShouldReturnEmailWhenCallToStringMethod() => Assert.Fail();
+    public class EmailTests
+    {
+        private const string ExpectedAddress = "emailtests@testes.com";
+        private readonly Mock<IDateTimeProvider> dateTimeProviderMock;
+
+        public EmailTests()
+        {
+            dateTimeProviderMock = new Mock<IDateTimeProvider>();
+        }
+
+        [Fact]
+        public void ShouldLowerCaseEmail()
+        {
+            // Arrange
+            var upperCasedAddress = "EmailTests@Testes.Com";
+
+            // Act
+            var email = Email.ShouldCreate(upperCasedAddress, dateTimeProviderMock.Object);
+
+            // Assert
+            Assert.Equal(ExpectedAddress, email.Address); 
+        }
+
+        [Fact]
+        public void ShouldTrimEmail()
+        {
+            // Arrange
+            var untrimmedAddress = " emailTests@testes.com ";
+
+            // Act
+            var email = Email.ShouldCreate(untrimmedAddress, dateTimeProviderMock.Object);
+
+            // Assert
+            Assert.Equal(ExpectedAddress, email.Address);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void ShouldFailIfEmailIsNullOrEmpty(string invalidAddress)
+        {
+            // Arrange
+            var dateTimeProviderMock = new Mock<IDateTimeProvider>();
+
+            // Act & Assert
+            var exception = Record.Exception(() =>
+                Email.ShouldCreate(invalidAddress ?? string.Empty, dateTimeProviderMock.Object)
+            );
+
+            // Assert
+            Assert.NotNull(exception);
+            Assert.IsType<InvalidEmailException>(exception);
+            Assert.Equal("E-mail inválido", exception.Message);
+        }
+
+        [Theory]
+        [InlineData("emailTests@@testes.com")]
+        [InlineData("emailTests@testes..com")]
+        [InlineData("emailTests@.com")]
+        [InlineData("emailTests@com")]
+        public void ShouldFailIfEmailIsInvalid(string invalidAddress)
+        {
+            // Act & Assert
+            Assert.Throws<InvalidEmailException>(() => Email.ShouldCreate(invalidAddress, dateTimeProviderMock.Object));
+        }
+
+        [Theory]
+        [InlineData("emailTests@testes.com")]
+        [InlineData("TestEmail@TESTES.COM")]
+        public void ShouldPassIfEmailIsValid(string validEmail)
+        {
+            // Act
+            var email = Email.ShouldCreate(validEmail, dateTimeProviderMock.Object);
+
+            // Assert
+            Assert.NotNull(email);
+            Assert.Equal(validEmail.ToLower(), email.Address); 
+        }
+
+
+        [Fact]
+        public void ShouldHashEmailAddress()
+        {
+            // Arrange
+            var email = Email.ShouldCreate(ExpectedAddress.ToLowerInvariant(), dateTimeProviderMock.Object); 
+            var expectedHash = ExpectedAddress.ToLowerInvariant().ToBase64(); 
+
+            // Act & Assert
+            Assert.Equal(expectedHash, email.Hash);
+        }
+
+
+        [Fact]
+        public void ShouldGenerateUniqueHashForDifferentEmails()
+        {
+            // Arrange
+            var email1 = Email.ShouldCreate("email1@testes.com", dateTimeProviderMock.Object);
+            var email2 = Email.ShouldCreate("email2@testes.com", dateTimeProviderMock.Object);
+
+            // Act & Assert
+            Assert.NotEqual(email1.Hash, email2.Hash);
+        }
+
+       
+        [Fact]
+        public void ShouldExplicitConvertFromString()
+        {
+            // Arrange
+            var emailString = "test@test.com";
+
+            // Act
+            var email = Email.ShouldCreate(emailString, dateTimeProviderMock.Object);
+
+            // Assert
+            Assert.Equal(emailString, email.Address);
+        }
+
+
+        [Fact]
+        public void ShouldExplicitConvertToString()
+        {
+            // Arrange
+            var emailString = "emailTests@testes.com"; 
+            var email = Email.ShouldCreate(emailString, dateTimeProviderMock.Object);
+
+            // Act
+            var result = (string)email;
+
+            // Assert
+            Assert.Equal(emailString.ToLower(), result); // Compare com a versão em minúsculas
+        }
+        [Fact]
+        public void ShouldReturnEmailWhenCallToStringMethod()
+        {
+            // Arrange
+            var email = Email.ShouldCreate(ExpectedAddress, dateTimeProviderMock.Object);
+
+            // Act
+            var result = email.ToString();
+
+            // Assert
+            Assert.Equal(ExpectedAddress, result);
+        }
+    }
 }
